a0351f64aafff1913ed2a4d4e2f55a1f3a5a75ea
Prabalatha K <prabalatha12k@gmail.com>
2026-03-17 17:52:25 +0530

    Update load_erp_customer.sql

--- Scripts/silver/load_silver/load_erp_customer.sql
+++ Scripts/silver/load_silver/load_erp_customer.sql
@@ -27,3 +27,33 @@ Target Table:
 
 ===============================================================================
 */
+
+USE DataWarehouse;
+
+-- Load ERP Customer
+
+INSERT INTO silver.erp_customer (
+    cid,
+    birth_date,
+    gender
+)
+SELECT
+    CASE 
+        WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+        ELSE cid
+    END,
+
+    CASE 
+        WHEN birth_date > GETDATE() THEN NULL
+        ELSE birth_date
+    END,
+
+    CASE 
+        WHEN UPPER(TRIM(gender)) IN ('F', 'Female') THEN 'Female'
+        WHEN UPPER(TRIM(gender)) IN ('M', 'Male') THEN 'Male'
+        ELSE 'N/A'
+    END
+
+FROM bronze.erp_customer;
+
+SELECT * FROM silver.erp_customer;
