f6041722d70d4c31692332ab6c4e9c3baad492ff
Prabalatha K <prabalatha12k@gmail.com>
2026-03-17 18:43:11 +0530

    Create load_crm_product_info.sql

---
+++ Scripts/bronze/load_bronze/load_crm_product_info.sql
@@ -0,0 +1,31 @@
+USE DataWarehouse;
+GO
+
+/*
+===============================================================================
+Load CRM Product Info (CSV → Bronze)
+===============================================================================
+*/
+
+CREATE OR ALTER PROCEDURE bronze.load_crm_product_info
+AS
+BEGIN
+
+    DECLARE @start_time DATETIME = GETDATE();
+
+    PRINT 'Loading: bronze.crm_product_info';
+
+    TRUNCATE TABLE bronze.crm_product_info;
+
+    BULK INSERT bronze.crm_product_info
+    FROM 'datasets/source_crm/product_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
+
+    PRINT 'Completed in ' + 
+    CAST(DATEDIFF(SECOND, @start_time, GETDATE()) AS VARCHAR) + ' seconds';
+
+END;
