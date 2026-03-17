13334d8a7e53e2f8a83a32ed00ae35dbb4fe3470
Prabalatha K <prabalatha12k@gmail.com>
2026-03-17 17:51:21 +0530

    Update load_crm_sales_details.sql

--- Scripts/silver/load_silver/load_crm_sales_details.sql
+++ Scripts/silver/load_silver/load_crm_sales_details.sql
@@ -30,3 +30,63 @@ Target Table:
 
 ===============================================================================
 */
+
+USE DataWarehouse;
+
+-- Load CRM Sales Details
+
+INSERT INTO silver.crm_sales_details (
+    sales_order_num,
+    sales_product_key,
+    sales_customer_id,
+    sales_order_date,
+    sales_ship_date,
+    sales_due_date,
+    sales_sales,
+    sales_quantity,
+    sales_price
+)
+SELECT
+    sales_order_num,
+    sales_product_key,
+    sales_customer_id,
+
+    -- Fix Invalid Dates
+    CASE 
+        WHEN sales_order_date = 0 OR LEN(sales_order_date) != 8 THEN NULL
+        ELSE CAST(CAST(sales_order_date AS VARCHAR) AS DATE)
+    END,
+
+    CASE 
+        WHEN sales_ship_date = 0 OR LEN(sales_ship_date) != 8 THEN NULL
+        ELSE CAST(CAST(sales_ship_date AS VARCHAR) AS DATE)
+    END,
+
+    CASE 
+        WHEN sales_due_date = 0 OR LEN(sales_due_date) != 8 THEN NULL
+        ELSE CAST(CAST(sales_due_date AS VARCHAR) AS DATE)
+    END,
+
+    -- Fix Sales Value
+    CASE 
+        WHEN sales_sales IS NULL 
+          OR sales_sales != sales_quantity * ABS(sales_price)
+        THEN sales_quantity * ABS(sales_price)
+        ELSE sales_sales
+    END,
+
+    sales_quantity,
+
+    -- Fix Price
+    CASE 
+        WHEN sales_price IS NULL OR sales_price <= 0
+        THEN sales_sales / NULLIF(sales_quantity, 0)
+        ELSE sales_price
+    END
+
+FROM bronze.crm_sales_details;
+
+---
+
+SELECT * FROM silver.crm_sales_details;
+
