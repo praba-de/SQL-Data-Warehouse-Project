e4e16115ea732b903da26cfefa27421a97d568ba
Prabalatha K <prabalatha12k@gmail.com>
2026-03-17 18:43:32 +0530

    Create load_crm_sales_details.sql

---
+++ Scripts/bronze/load_bronze/load_crm_sales_details.sql
@@ -0,0 +1,31 @@
+USE DataWarehouse;
+GO
+
+/*
+===============================================================================
+Load CRM Sales Details (CSV → Bronze)
+===============================================================================
+*/
+
+CREATE OR ALTER PROCEDURE bronze.load_crm_sales_details
+AS
+BEGIN
+
+    DECLARE @start_time DATETIME = GETDATE();
+
+    PRINT 'Loading: bronze.crm_sales_details';
+
+    TRUNCATE TABLE bronze.crm_sales_details;
+
+    BULK INSERT bronze.crm_sales_details
+    FROM 'datasets/source_crm/sales_details.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
+
+    PRINT 'Completed in ' + 
+    CAST(DATEDIFF(SECOND, @start_time, GETDATE()) AS VARCHAR) + ' seconds';
+
+END;
