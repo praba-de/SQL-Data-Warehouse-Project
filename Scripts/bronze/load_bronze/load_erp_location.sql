bf1f9f9a6822c520b0f641937f8c8cfcb907af8d
Prabalatha K <prabalatha12k@gmail.com>
2026-03-17 18:44:06 +0530

    Create load_erp_location.sql

---
+++ Scripts/bronze/load_bronze/load_erp_location.sql
@@ -0,0 +1,31 @@
+USE DataWarehouse;
+GO
+
+/*
+===============================================================================
+Load ERP Location (CSV → Bronze)
+===============================================================================
+*/
+
+CREATE OR ALTER PROCEDURE bronze.load_erp_location
+AS
+BEGIN
+
+    DECLARE @start_time DATETIME = GETDATE();
+
+    PRINT 'Loading: bronze.erp_location';
+
+    TRUNCATE TABLE bronze.erp_location;
+
+    BULK INSERT bronze.erp_location
+    FROM 'datasets/source_erp/LOCATION.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
+
+    PRINT 'Completed in ' + 
+    CAST(DATEDIFF(SECOND, @start_time, GETDATE()) AS VARCHAR) + ' seconds';
+
+END;
