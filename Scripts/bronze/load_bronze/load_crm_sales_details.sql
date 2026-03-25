1c1e1859f5d7da9dbe209e3d8489a3ec0f560539
Prabalatha K <prabalatha12k@gmail.com>
2026-03-25 15:53:23 +0530

    Update load_crm_sales_details.sql

--- Scripts/bronze/load_bronze/load_crm_sales_details.sql
+++ Scripts/bronze/load_bronze/load_crm_sales_details.sql
@@ -7,15 +7,13 @@ Load CRM Sales Details (CSV → Bronze)
 ===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_crm_sales_details
+CREATE PROCEDURE bronze.load_crm_sales_details
 AS
 BEGIN
 
-    DECLARE @start_time DATETIME = GETDATE();
+    DECLARE @start_time = GETDATE();
 
     PRINT 'Loading: bronze.crm_sales_details';
-
-    TRUNCATE TABLE bronze.crm_sales_details;
 
     BULK INSERT bronze.crm_sales_details
     FROM 'datasets/source_crm/sales_details.csv'
@@ -25,7 +23,7 @@ BEGIN
         TABLOCK
     );
 
-    PRINT 'Completed in ' + 
+    PRINT 'Completed... ' + 
     CAST(DATEDIFF(SECOND, @start_time, GETDATE()) AS VARCHAR) + ' seconds';
 
 END;
